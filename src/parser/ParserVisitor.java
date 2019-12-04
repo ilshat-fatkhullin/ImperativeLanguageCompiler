57d417e71995707f50747bcf06118bc0d90ecb78
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2019-12-05 01:16:40 +0300

    Prepared AST traverse methods for .NET CIL conversion.

--- src/parser/ParserVisitor.java
+++ src/parser/ParserVisitor.java
@@ -2,16 +2,319 @@ package parser;
 
 import i_grammar.IBaseVisitor;
 import i_grammar.IParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
 
 public class ParserVisitor extends IBaseVisitor<String> {
-
     @Override
-    public String visitProgram(IParser.ProgramContext ctx) {
-        return "program \n" + visitChildren(ctx);
+    public String visitChildren(RuleNode node) {
+        String result = "";
+        int n = node.getChildCount();
+
+        for(int i = 0; i < n && this.shouldVisitNextChild(node, result); ++i) {
+            ParseTree c = node.getChild(i);
+            String childResult = c.accept(this);
+            result += childResult;
+        }
+
+        return result;
     }
 
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitProgram(IParser.ProgramContext ctx) {
+        return "program " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitSimple_declaration(IParser.Simple_declarationContext ctx) {
+        return "simple_declaration " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitVariable_declaration(IParser.Variable_declarationContext ctx) {
+        return "variable_declaration " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitType_declaration(IParser.Type_declarationContext ctx) {
+        return "type_declaration " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
     @Override
     public String visitRoutine_declaration(IParser.Routine_declarationContext ctx) {
-        return "routine \n" + visitChildren(ctx);
+        return "routine_declaration " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitParameters(IParser.ParametersContext ctx) {
+        return "parameters " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitParameter_declaration(IParser.Parameter_declarationContext ctx) {
+        return "parameter_declaration " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitType(IParser.TypeContext ctx) {
+        return "type " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitPrimitive_type(IParser.Primitive_typeContext ctx) {
+        return "primitive_type " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitRecord_type(IParser.Record_typeContext ctx) {
+        return "record_type " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitArray_type(IParser.Array_typeContext ctx) {
+        return "array_type " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitBody(IParser.BodyContext ctx) {
+        return "body " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitStatement(IParser.StatementContext ctx) {
+        return "statement " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitAssignment(IParser.AssignmentContext ctx) {
+        return "assignment " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitRoutine_call(IParser.Routine_callContext ctx) {
+        return "routine_call " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitWhile_loop(IParser.While_loopContext ctx) {
+        return "while_loop " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitFor_loop(IParser.For_loopContext ctx) {
+        return "for_loop " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitRange(IParser.RangeContext ctx) {
+        return "range " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitForeach_loop(IParser.Foreach_loopContext ctx) {
+        return "foreach_loop " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitIf_statement(IParser.If_statementContext ctx) {
+        return "if_statement " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitExpression(IParser.ExpressionContext ctx) {
+        return "expression " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitRelation(IParser.RelationContext ctx) {
+        return "relation " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitSimple(IParser.SimpleContext ctx) {
+        return "simple " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitFactor(IParser.FactorContext ctx) {
+        return "factor " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitSummand(IParser.SummandContext ctx) {
+        return "summand " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitPrimary(IParser.PrimaryContext ctx) {
+        return "primary " + visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public String visitModifiable_primary(IParser.Modifiable_primaryContext ctx) {
+        return "modifiable_primary " + visitChildren(ctx);
     }
 }
